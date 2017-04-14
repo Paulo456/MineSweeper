c6a89d6dadb2328acbc1eb3f70dc3f9b5620ebf2
cjay <cjaym@yandex>
2017-04-14 11:16:40 +0300

    подправил названия переменных

--- ui/start_window.py
+++ ui/start_window.py
@@ -13,51 +13,51 @@ class StartWindow(object):
         self.window.title('Настройки')
         self.window.geometry('200x150')
 
-        self.mineText = None
-        self.highText = None
-        self.lenghtText = None
+        self.mine_input = None
+        self.width_input = None
+        self.height_input = None
 
         self.create_ui()
 
     def create_ui(self):
-        self.mineText = Text(self.window, width=5, height=1)  # Создаем поля для ввода текста и пояснения
-        self.mineText.insert(END, self.mines)
-        self.mineText.place(x=75, y=5)
+        self.mine_input = Text(self.window, width=5, height=1)  # Создаем поля для ввода текста и пояснения
+        self.mine_input.insert(END, self.mines)
+        self.mine_input.place(x=75, y=5)
 
-        mineLabe = Label(self.window, height=1, text='Бомбы:')
-        mineLabe.place(x=5, y=5)
+        mines_label = Label(self.window, height=1, text='Бомбы:')
+        mines_label.place(x=5, y=5)
 
-        self.highText = Text(self.window, width=5, height=1)
-        self.highText.insert(END, self.width)
-        self.highText.place(x=75, y=30)
+        self.width_input = Text(self.window, width=5, height=1)
+        self.width_input.insert(END, self.width)
+        self.width_input.place(x=75, y=30)
 
-        highLabe = Label(self.window, height=1, text='Ширина:')
-        highLabe.place(x=5, y=30)
+        width_label = Label(self.window, height=1, text='Ширина:')
+        width_label.place(x=5, y=30)
 
-        self.lenghtText = Text(self.window, width=5, height=1)
-        self.lenghtText.insert(END, self.height)
-        self.lenghtText.place(x=75, y=55)
+        self.height_input = Text(self.window, width=5, height=1)
+        self.height_input.insert(END, self.height)
+        self.height_input.place(x=75, y=55)
 
-        lenghtLabe = Label(self.window, height=1, text='Высота:')
-        lenghtLabe.place(x=5, y=55)
+        height_label = Label(self.window, height=1, text='Высота:')
+        height_label.place(x=5, y=55)
 
-        self.mineBut = Button(self.window, text='Начать:', command=self.start_game)
-        self.mineBut.place(x=70, y=90)
+        button = Button(self.window, text='Начать:', command=self.start_game)
+        button.place(x=70, y=90)
 
     def run(self):
         self.window.mainloop()
 
     def validate_input(self):
-        if self.mineText.get('1.0', END) != '\n':
-            self.mines = int(self.mineText.get('1.0', END))
+        if self.mine_input.get('1.0', END) != '\n':
+            self.mines = int(self.mine_input.get('1.0', END))
 
-        if self.highText.get('1.0', END) != '\n':
-            self.width = int(self.highText.get('1.0', END))
+        if self.width_input.get('1.0', END) != '\n':
+            self.width = int(self.width_input.get('1.0', END))
 
-        if self.lenghtText.get('1.0', END) != '\n':
-            self.height = int(self.lenghtText.get('1.0', END))
+        if self.height_input.get('1.0', END) != '\n':
+            self.height = int(self.height_input.get('1.0', END))
 
     def start_game(self):
         self.validate_input()
-        mineWindow = GameWindow(self.width, self.height, self.mines)
-        mineWindow.run()
+        game_window = GameWindow(self.width, self.height, self.mines)
+        game_window.run()
